0bce37b5e4f98f1540b64f2242a163304d900e21
mata0050 <mata0050@outlook.com>
2022-03-25 13:06:23 -0400

    pose redux setup

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import poseReducer from '../features/pose/poseSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    pose: poseReducer,
   },
 });
