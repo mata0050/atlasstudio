03a92bc24410ab3efe00e6d242d32db4a6003fe7
mata0050 <mata0050@outlook.com>
2022-03-25 23:58:49 -0400

    Pose main page component styled

--- client/src/pages/MainPage/Pose.jsx
+++ client/src/pages/MainPage/Pose.jsx
@@ -18,19 +18,54 @@ function Pose() {
   return (
     <StyledPose>
       {allPoses.map((pose) => (
-        <div>
+        <div className='pose'>
           <h2>{pose.title}</h2>
           <div className='pose-container'>
             <img src={pose.pose_image} alt={pose.title} />
-            {parse(pose.pose_description)}
+            <div className='description'>
+              {parse(pose.pose_description)}
+
+              <h3>Description</h3>
+              {parse(pose.description)}
+            </div>
           </div>
-          {parse(pose.see_also)}
         </div>
       ))}
     </StyledPose>
   );
 }
 
-const StyledPose = styled.div``;
+const StyledPose = styled.div`
+  .pose {
+    margin: 20px 0;
+    border: 1px solid #ccc;
+    h2 {
+      background-color: var(--color-brown);
+      padding: 10px;
+      font-size: 1rem;
+    }
+
+    .pose-container {
+      padding: 10px;
+      display: flex;
+
+      img {
+        height: 150px;
+        width: 250px;
+        object-fit: cover;
+      }
+
+      .description {
+        max-height: 150px;
+        padding-left: 20px;
+        overflow: hidden;
+        h3 {
+          margin: 10px 0;
+          font-size: 1.1rem;
+        }
+      }
+    }
+  }
+`;
 
 export default Pose;
